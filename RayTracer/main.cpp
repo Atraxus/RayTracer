631f98cc1e0faef67fd60f5982c52d75cf4cb1bb
Jannis Maier <jannismaier97@gmail.com>
2020-01-28 15:33:54 +0100

    All the new things

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -12,7 +12,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
