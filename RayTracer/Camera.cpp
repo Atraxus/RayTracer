63c34123ecd072425030a860eaff271fbe6cbff6
EnricoAhlers <enrico@anolix.de>
2020-02-07 16:59:18 +0100

    added camera

--- RayTracer/Camera.cpp
+++
@@ -1,9 +0,0 @@
-#include "Camera.h"
-
-Camera::Camera():viewDirection(0.0f, 0.0f, -1.0f) {
-
-}
-
-glm::mat4 Camera::worldToView() const {
-	return glm::lookAt(position, position + viewDirection, glm::vec3(0.0f, 1.0f, 0.0f));
-}
